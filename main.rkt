73221c8e82bdeb0692a84f61e68b4a8f07f0e1d5
Subhajit Sahu <wolfram77@gmail.com>
2022-02-02 17:00:04 +0530

    :turtle: update name

--- main.rkt
+++ main.rkt
@@ -1,11 +1,11 @@
 #lang racket
 
-; (down lst): List(N) -> List(List)
+; (slist.down lst): List(N) -> List(List)
 ; wraps parentheses around each top-level element of lst
 ; : size(lst)=0 -> ()
-; : else        -> ((lst[0]) (down lst[1..end]))
-(define down
+; : else        -> ((lst[0]) (slist.down lst[1..end]))
+(define slist.down
   (lambda (lst)
     (if (null? lst)
         (list)
-        (cons (list (car lst)) (down (cdr lst))))))
+        (cons (list (car lst)) (slist.down (cdr lst))))))
